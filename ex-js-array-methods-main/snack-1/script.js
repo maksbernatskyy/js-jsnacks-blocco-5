5815fdc6aa21c1ddacea8aeb03cd0137434ee180
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-22 13:41:13 +0200

    complete snack 1

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -1,4 +1,18 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
+
+// Semplice console.log
+console.log(names)
+
+// Metodo for
+for (let i = 0; i < names.length; i++) {
+    const thisName = names[i]
+    console.log(thisName)
+}
+
+// Metodo forEach
+names.forEach((thisName) => {
+    console.log(thisName)
+})
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
