aaea985a5c06f31cc14eb8f8edb7e6a069283bbf
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-23 09:30:18 +0200

    fix task 3

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -4,15 +4,24 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Metodo for
 console.log('Metodo for')
+
+const numIncrement = []
+console.log(numIncrement)
+
 for (let i = 0; i < numbers.length; i++) {
     const thisNumber = numbers[i] + 1
-    console.log(thisNumber)
+    numIncrement.push(thisNumber)
 }
 
 // Metodo forEach
 console.log('Metodo forEach')
+
+const numForEachIncrement = []
+console.log(numForEachIncrement)
+
 numbers.forEach((thisNumber) => {
-    console.log(thisNumber + 1)
+    thisNumberPlusOne = thisNumber + 1
+    numForEachIncrement.push(thisNumberPlusOne)
 })
 
 // Risultato: [3, 9, 5, 8, 3, 88]
